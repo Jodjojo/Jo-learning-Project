0b56e6fb20685a8756499e02e5a2474d11796085
Joseph Yusuff <joseph.yusuff12@gmail.com>
2023-07-07 16:20:55 +0100

    User Router bugfix

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,6 +2,6 @@ const express = require("express");
 const userController = require("../controllers/userController");
 const router = express.Router();
 
-router.post("/signup", userController.signUp);
+router.post("/signup", userController.signup);
 
 module.exports = router;

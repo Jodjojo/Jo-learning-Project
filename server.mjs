409139f0f4f0de3da5d00f23538df81c2cd72417
Joseph Yusuff <joseph.yusuff12@gmail.com>
2023-07-07 20:50:24 +0100

    API repo and base application using ECMA scripts

---
+++ server.mjs
@@ -0,0 +1,18 @@
+import express from "express";
+import bodyParser from "body-parser";
+const app = express();
+const port = 3000;
+
+// Middleware to prase JSON to request bodies
+app.use(bodyParser.json());
+
+// Base Route
+app.get("/", (req, res) => {
+	res.send("Welcome to the API!");
+});
+
+// API routes
+
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}...`);
+});

312888ecc52e8c6e2d6395f6259011d5b5774085
Joseph Yusuff <joseph.yusuff12@gmail.com>
2023-07-10 01:57:39 +0100

    ECMA

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-
-const app = express();
-const port = 3000;
-
-app.use(bodyParser.json());
-
-// server
-app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
-});
 import express from "express";
 import bodyParser from "body-parser";
 const app = express();

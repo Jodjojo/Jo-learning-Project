8ad46d50be59234c55ac0933013d7a191da14a80
Joseph Yusuff <joseph.yusuff12@gmail.com>
2023-07-10 20:36:32 +0100

    DB setup and migrations for DB creation

---
+++ knexfile.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+	client: "pg",
+	connection: {
+		user: "postgres",
+		password: "Manutd12",
+		host: "127.0.0.1",
+		database: "postgres",
+	},
+	migrations: {
+		directory: "./migrations",
+	},
+};

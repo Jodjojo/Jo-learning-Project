a1c1e300562cd729ccf1efae5d69aad965a773cb
Joseph Yusuff <joseph.yusuff12@gmail.com>
2023-07-12 21:44:33 +0100

    Cleanup in main branch

--- routes/userRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const userController = require("../controllers/userController");
-
-const Router = express.Router();
-
-Router.post("/signup", userController.signUp);

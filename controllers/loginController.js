8a76c7a95e126ce6529b30ad420be6fb737d53a0
Joseph Yusuff <joseph.yusuff12@gmail.com>
2023-07-13 09:08:44 +0100

    Login API using MVC architecture and postgreSQL

---
+++ controllers/loginController.js
@@ -0,0 +1,26 @@
+import pool from "../db.js";
+
+export const login = async (req, res) => {
+	const { email, password } = req.body;
+
+	try {
+		const result = await pool.query("SELECT * FROM users WHERE email = $1", [
+			email,
+		]);
+
+		if (result.rows.length === 0) {
+			return res.status(404).json({ error: "User not found" });
+		}
+
+		const user = result.rows[0];
+
+		if (user.password !== password) {
+			return res.status(401).json({ error: "Incorrect password" });
+		}
+
+		return res.status(200).json({ message: "Login successful", user });
+	} catch (error) {
+		console.error("Error during login:", error);
+		return res.status(500).json({ error: "Internal server error" });
+	}
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { signup } from "../controllers/userController.js";
-
+import { login } from "../controllers/loginController.js";
 const router = express.Router();
 
 router.post("/signup", signup);
+
+// login route
+router.get("/login", login);
 
 export default router;
